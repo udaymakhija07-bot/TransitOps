c2bfbc6d7018f34fcbc52e68155534cc22face9a
Uday Makhija <udaymakhija@Udays-MacBook-Air.local>
2026-07-12 09:57:12 +0530

    feat: setup basic Express boilerplate and scripts

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Base Route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the TransitOps API' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+module.exports = app;
